1772eb3a46641bb7771ef43f43d6969897a07fee
erhodes <erik@space-nav.com>
2023-04-21 11:46:57 -0600

    bit test code complete

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
-use sea_orm;
+use sea_orm::prelude::*;
+mod entity;
+use entity::bit_test;
 
-fn main() {
-    println!("Hello, world!");
+
+#[tokio::main]
+async fn main() {
+    let conn = sea_orm::Database::connect(std::env::var("DATABASE_URL").unwrap()).await.unwrap();
+    let query = bit_test::Entity::find();
+    let results = query.clone().all(&conn).await.unwrap();
+    let results_json = query.into_json().all(&conn).await.unwrap();
+
+    dbg!(&results);
+    dbg!(&results_json);
+    assert!(!results.is_empty());
+    for result in results_json {
+        assert!(result.get("field").is_some())
+    }
 }
---
+++ table.sql
@@ -0,0 +1,4 @@
+CREATE TABLE `bit_test` (
+  `field` bit(1) NOT NULL,
+  PRIMARY KEY (`field`)
+)
