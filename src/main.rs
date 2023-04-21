b7e235f21116875e7a685c5ab563cfd6cf0759f8
erhodes <erik@space-nav.com>
2023-04-21 11:48:17 -0600

    cleaned up main as a template

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,8 @@
 use sea_orm::prelude::*;
 mod entity;
-use entity::bit_test;
 
 
 #[tokio::main]
 async fn main() {
     let conn = sea_orm::Database::connect(std::env::var("DATABASE_URL").unwrap()).await.unwrap();
-    let query = bit_test::Entity::find();
-    let results = query.clone().all(&conn).await.unwrap();
-    let results_json = query.into_json().all(&conn).await.unwrap();
-
-    dbg!(&results);
-    dbg!(&results_json);
-    assert!(!results.is_empty());
-    for result in results_json {
-        assert!(result.get("field").is_some())
-    }
 }
--- table.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE `bit_test` (
-  `field` bit(1) NOT NULL,
-  PRIMARY KEY (`field`)
-)
